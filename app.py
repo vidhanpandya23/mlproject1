c9bcad62fe7ef9c81d8a208194e5d6b8ee873980
vidhanpandya23 <vidhanpandya995@gmail.com>
2023-12-02 15:51:19 +0530

    Model Trainer

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src.genericml.exception import CustomException
 from src.genericml.components.data_ingestion import DataIngestion
 from src.genericml.components.data_ingestion import DataIngestionConfig
 from src.genericml.components.data_transformation import DataTransformationConfig, DataTransformation
-
+from src.genericml.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 import sys
 
@@ -18,7 +18,12 @@ if __name__=="__main__":
 
         # data_transformation_config=DataIngestionConfig()
         data_transformation =DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+        ## Model Training
+
+        model_trainer =ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
     except Exception as e:
         raise CustomException(e,sys)
